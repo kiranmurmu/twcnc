a96213c74f5fc04b500e8e59cde746da580e8326
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2022-01-22 13:20:42 +0530

    chore: lots of codes

--- src/__test__/twcnc.test.ts
+++ src/__test__/twcnc.test.ts
@@ -1,5 +1,9 @@
 import twcnc from "../twcnc";
 
+test("Test 00: Input type - all | check if value is empty", () => {
+  expect(twcnc("", [], {})).toBe("");
+});
+
 test("Test 01: Input type - string | multiple class names", () => {
-  expect(twcnc("class1", "class2")).toBe("class1 class2");
+  expect(twcnc("class1", "class2", ["class3"])).toBe("class1 class2");
 });
--- src/twcnc.ts
+++ src/twcnc.ts
@@ -1,11 +1,114 @@
-type twcArg = Array<string | Array<string>>;
+interface twcobj {
+  [name: string]: string | boolean | string[];
+}
+type argv = string | twcobj | string[];
+interface twc {
+  clean: (_val: string) => string;
+  split: (_val: string) => string[];
+}
+/*
+isMod: (_val: string) => boolean;
+check: (_val: argv) => boolean;
 
-function twcnc(...twcArg: twcArg): string {
-  const _res: Array<string> = [];
-  twcArg.forEach((_arg) => {
-    if (typeof _arg === "string") _res.push(_arg);
-  });
-  return _res.join(" ").trim();
+function clean(_val: string) {
+  return _val.replace(/\s\s+/g, " ").trim();
+}
+
+function split(_val: string) {
+  return clean(_val).split(" ");
+}
+*/
+const _twc: twc = {
+  clean: (_val) => _val.replace(/\s\s+/g, " ").trim(),
+  split: (_val) => _twc.clean(_val).split(" "),
+  /*
+  isMod: (_val) => {
+    if (
+      !!_val &&
+      !_val.includes(" ") &&
+      _val.endsWith(":") &&
+      !_val.startsWith(":")
+    )
+      return true;
+    else return false;
+  },
+
+  check: (_val) => {
+    switch (typeof _val) {
+      case "object":
+        return !!_val && Array.isArray(_val)
+          ? !!_val.length
+          : !!Object.keys(_val).length;
+      case "string":
+        return !!_val;
+      default:
+        return false;
+    }
+  },*/
+};
+
+function reduce(_arg: argv): string[] {
+  const _res: string[] = [""];
+  switch (typeof _arg) {
+    case "string":
+      if (!!_arg) return _twc.split(_arg);
+      break;
+    case "object":
+      if (Array.isArray(_arg)) {
+        for (const _val of _arg) {
+          if (!!_val) _res.push(..._twc.split(_val));
+        }
+      } else {
+        for (const [_idx, _val] of Object.entries(_arg)) {
+          const _key: string = _twc.clean(_idx);
+          if (!!_key) {
+            if (typeof _val === "string") {
+              if (
+                !_key.includes(" ") &&
+                _key.endsWith(":") &&
+                !_key.startsWith(":")
+              ) {
+                for (const _cnv of _twc.split(_val)) {
+                  _res.push(_key + _cnv);
+                }
+              } else {
+                _res.push(_key, ..._twc.split(_val));
+              }
+            } else if (typeof _val === "object") {
+              _res.push(...reduce(_val));
+            } else {
+              if (_val) _res.push(..._twc.split(_key));
+            }
+          }
+        }
+      }
+      break;
+  }
+  return _res;
+}
+
+function twcnc(...args: argv[]): string {
+  // const _res: string[] = [];
+  return !!args.length
+    ? args
+        .map((_arg) => {
+          return reduce(_arg);
+        })
+        .join(" ")
+        .trim()
+    : "";
+
+  /*for (const _arg of args) {
+    if (
+      (typeof _arg === "string" && !!_arg) ||
+      (typeof _arg === "object" && Array.isArray(_arg)
+        ? !!_arg.length
+        : !!Object.keys(_arg).length)
+    ) {
+      _res.push(...reduce(_arg));
+    }
+  }*/
+  // return _res.join(" ").trim();
 }
 
 export default twcnc;
