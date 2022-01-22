7b47ccdc905b095cde98e403d62c78a7f9101cba
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2022-01-22 13:33:25 +0530

    test: new normal class names

--- src/__test__/twcnc.test.ts
+++ src/__test__/twcnc.test.ts
@@ -4,6 +4,8 @@ test("Test 00: Input type - all | check if value is empty", () => {
   expect(twcnc("", [], {})).toBe("");
 });
 
-test("Test 01: Input type - string | multiple class names", () => {
-  expect(twcnc("class1", "class2", ["class3"])).toBe("class1 class2");
+test("Test 01: Input type - string | multiple normal class names", () => {
+  expect(twcnc("text-xl font-medium text-black")).toBe(
+    "text-xl font-medium text-black"
+  );
 });
