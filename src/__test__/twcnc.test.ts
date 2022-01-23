a9bac8a94908a9337cbcd9239b494b84dcddd76f
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2022-01-23 07:20:16 +0530

    test: array & object class test

--- src/__test__/twcnc.test.ts
+++ src/__test__/twcnc.test.ts
@@ -1,11 +1,39 @@
 import twcnc from "../twcnc";
 
-test("Test 00: Input type - all | check if value is empty", () => {
+test("Test 00: Input type - all | invalid classes", () => {
   expect(twcnc("", [], {})).toBe("");
 });
 
-test("Test 01: Input type - string | multiple normal class names", () => {
-  expect(twcnc("text-xl font-medium text-black")).toBe(
-    "text-xl font-medium text-black"
+test("Test 01: Input type - string | string classes", () => {
+  expect(
+    twcnc(
+      "text-xl font-medium text-black",
+      "shadow-lg flex items-center space-x-4"
+    )
+  ).toBe(
+    "text-xl font-medium text-black shadow-lg flex items-center space-x-4"
+  );
+});
+
+test("Test 02: Input type - string array | string array classes", () => {
+  expect(
+    twcnc([
+      "text-xl font-medium text-black",
+      "shadow-lg flex items-center space-x-4",
+    ])
+  ).toBe(
+    "text-xl font-medium text-black shadow-lg flex items-center space-x-4"
+  );
+});
+
+test("Test 03: Input type - object | key, value classes", () => {
+  expect(
+    twcnc({
+      "focus:": "text-xl font-medium text-black",
+      "hover:": "shadow-lg flex items-center space-x-4",
+    })
+  ).toBe(
+    "focus:text-xl focus:font-medium focus:text-black " +
+      "hover:shadow-lg hover:flex hover:items-center hover:space-x-4"
   );
 });
