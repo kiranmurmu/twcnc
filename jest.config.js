12221c61336d17c1dd69097be1777dbad5f0c33e
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2022-01-26 09:08:49 +0530

    chore: config babel and benchmark

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transform: {
     "\\.[jt]sx?$": "ts-jest",
   },
-  transformIgnorePatterns: ["/node_modules/", "/lib/", "/coverage/"],
+  transformIgnorePatterns: ["/node_modules/", "/lib/", "/coverage/", "/bench/"],
 };
