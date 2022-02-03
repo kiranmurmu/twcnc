82c62db617ad8c3a6e2daf9a9cb09addcbd5c002
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2022-02-04 04:50:48 +0530

    chore: improve codes

--- src/twcnc.ts
+++ src/twcnc.ts
@@ -1,99 +1,111 @@
-interface twcobj {
-  [name: string]: string | boolean | string[];
-}
-type argv = string | twcobj | string[];
-interface twc {
-  clean: (_val: string) => string;
-  split: (_val: string) => string[];
-}
-
-const _twc: twc = {
-  clean: (_val) => _val.replace(/\s\s+/g, " ").trim(),
-  split: (_val) => _twc.clean(_val).split(" "),
+type cobj = {
+  [key: string]: string | boolean | string[];
 };
+type argv = string | string[] | cobj;
 
-function _cn_str(_val: string): string[] {
-  const _res: string = _val.replace(/\s\s+/g, " ").trim();
-  if (!!_res) return _res.split(" ");
-  return [""];
+function _cn_cln(_val: string): string {
+  return _val.replace(/\s\s+/g, " ").trim();
 }
 
-function _cn_obj(_ent: string[] | twcobj): string {
+function _cn_spl(_val: string): string[] {
+  const _res: string = _val.replace(/\s\s+/g, " ").trim();
+
+  return _res.split(" ");
+}
+
+function _cn_str(_key: string, _val: string): string {
   let _res = "";
-  if (Array.isArray(_ent)) {
-    for (const _val of _ent) {
-      if (!_val) continue;
+
+  if (!_key.includes(" ") && _key.endsWith(":") && !_key.startsWith(":")) {
+    for (const _cnv of _cn_spl(_val)) {
       if (_res) _res += " ";
-      _res += _val;
+      _res += _key + _cnv;
     }
   } else {
-    for (const _key in _ent) {
-      if (!_ent[_key] || !_key) continue;
+    if (_key) _res += _key;
+
+    for (const _cnv of _cn_spl(_val)) {
       if (_res) _res += " ";
-      _res += _ent[_key];
+      _res += _cnv;
     }
   }
+
   return _res;
 }
 
-function _cn_mix(_arg: argv): string[] {
-  const _res: string[] = [];
-  switch (typeof _arg) {
-    case "string":
-      _res.push(..._cn_str(_arg));
-      break;
-    case "object":
-      if (Array.isArray(_arg)) {
-        for (const _val of _arg) {
-          if (!!_val) _res.push(..._twc.split(_val));
-        }
-      } else {
-        for (const [_idx, _val] of Object.entries(_arg)) {
-          const _key: string = _twc.clean(_idx);
-          if (!!_key) {
-            if (typeof _val === "string") {
-              if (
-                !_key.includes(" ") &&
-                _key.endsWith(":") &&
-                !_key.startsWith(":")
-              ) {
-                for (const _cnv of _twc.split(_val)) {
-                  _res.push(_key + _cnv);
-                }
-              } else {
-                _res.push(..._twc.split(_key + " " + _val));
-              }
-            } else if (typeof _val === "object" && Array.isArray(_val)) {
-              const _arr: string[] = _cn_mix(_val);
-              if (
-                !_key.includes(" ") &&
-                _key.endsWith(":") &&
-                !_key.startsWith(":")
-              ) {
-                for (const _cnv of _arr) {
-                  _res.push(_key + _cnv);
-                }
-              } else {
-                _arr.unshift(_key);
-                _res.push(..._arr);
-              }
-            } else {
-              if (_val) _res.push(..._twc.split(_key));
-            }
-          }
-        }
+function _cn_arr(_key: string, _val: string[]): string {
+  let _res = "";
+
+  if (!_key.includes(" ") && _key.endsWith(":") && !_key.startsWith(":")) {
+    for (const _cni of _val) {
+      for (const _cnv of _cn_spl(_cni)) {
+        if (_res) _res += " ";
+        _res += _key + _cnv;
       }
-      break;
+    }
+  } else {
+    if (_key) _res += _key;
+
+    for (const _cni of _val) {
+      for (const _cnv of _cn_spl(_cni)) {
+        if (_res) _res += " ";
+        _res += _cnv;
+      }
+    }
   }
+
   return _res;
+}
+
+function _cn_obj(_ent: string[] | cobj): string {
+  let _res = "";
+
+  if (Array.isArray(_ent)) {
+    _res += _cn_arr("", _ent);
+  } else {
+    for (const _idx in _ent) {
+      const _val = _ent[_idx];
+      const _key = _cn_cln(_idx);
+
+      switch (typeof _val) {
+        case "string":
+          if (_res) _res += " ";
+          _res += _cn_str(_key, _val);
+          break;
+
+        case "object":
+          if (_res) _res += " ";
+          _res += _cn_arr(_key, _val);
+          break;
+
+        default:
+          if (!_val || !_key) break;
+          _res += _cn_str("", _key);
+          break;
+      }
+    }
+  }
+
+  return _res;
+}
+
+function _cn_mix(_arg: argv): string {
+  if (typeof _arg === "object") {
+    return _cn_obj(_arg);
+  } else {
+    return _cn_cln(_arg);
+  }
 }
 
 function twcnc(...args: argv[]): string {
-  const _res: string[] = [];
+  let _res = "";
+
   for (const _arg of args) {
-    _res.push(..._cn_mix(_arg));
+    if (_res) _res += " ";
+    _res += _cn_mix(_arg);
   }
-  return _res.join(" ").trim();
+
+  return _res.trim();
 }
 
 export default twcnc;
