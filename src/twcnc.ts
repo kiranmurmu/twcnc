dac60736ee424d1e29c5d899e7b27529e91a3aa5
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2022-02-06 09:52:25 +0530

    chore: bug fix & performance improve

--- src/twcnc.ts
+++ src/twcnc.ts
@@ -6,28 +6,8 @@ type argv = string | string[] | cobj;
 function _cn_cln(_val: string): string {
   let _res = "";
 
-  for (const _cnv of _val.split(" ")) {
-    if (!_cnv) continue;
-    if (_res) _res += " ";
-    _res += _cnv;
-  }
-
-  return _res;
-}
-
-function _cn_str(_key: string, _val: string[]): string {
-  let _res = "";
-
-  if (!_key.includes(" ") && _key.endsWith(":") && !_key.startsWith(":")) {
-    for (const _cnv of _val) {
-      if (!_cnv) continue;
-      if (_res) _res += " ";
-      _res += _key + _cnv;
-    }
-  } else {
-    if (_key) _res += _key;
-
-    for (const _cnv of _val) {
+  if (_val) {
+    for (const _cnv of _val.split(" ")) {
       if (!_cnv) continue;
       if (_res) _res += " ";
       _res += _cnv;
@@ -37,26 +17,84 @@ function _cn_str(_key: string, _val: string[]): string {
   return _res;
 }
 
-function _cn_arr(_key: string, _val: string[]): string {
+function _cn_str(_key: string, _val: string): string {
   let _res = "";
+  let _tmp = "";
 
-  if (!_key.includes(" ") && _key.endsWith(":") && !_key.startsWith(":")) {
-    for (const _cni of _val) {
-      for (const _cnv of _cni.split(" ")) {
+  if (_key && !_key.includes(" ") && _key.endsWith(":")) {
+    if (_val) {
+      for (const _cnv of _val.split(" ")) {
         if (!_cnv) continue;
         if (_res) _res += " ";
         _res += _key + _cnv;
       }
     }
   } else {
-    if (_key) _res += _key;
-
-    for (const _cni of _val) {
-      for (const _cnv of _cni.split(" ")) {
+    if (_val) {
+      for (const _cnv of _val.split(" ")) {
         if (!_cnv) continue;
-        if (_res) _res += " ";
-        _res += _cnv;
+        if (_tmp) _tmp += " ";
+        _tmp += _cnv;
       }
+    }
+
+    if (_tmp) {
+      if (_key) _res += _key;
+      if (_res) _res += " ";
+      _res += _tmp;
+    }
+  }
+
+  return _res;
+}
+
+function _cn_arr(_key: string, _val: string[]): string {
+  let _res = "";
+  let _tmp = "";
+
+  if (_key && !_key.includes(" ") && _key.endsWith(":")) {
+    if (_val) {
+      for (const _cni of _val) {
+        if (!_cni) continue;
+
+        for (const _cnv of _cni.split(" ")) {
+          if (!_cnv) continue;
+          if (_res) _res += " ";
+          _res += _key + _cnv;
+        }
+      }
+    }
+  } else {
+    if (_val) {
+      for (const _cni of _val) {
+        if (!_cni) continue;
+
+        for (const _cnv of _cni.split(" ")) {
+          if (!_cnv) continue;
+          if (_tmp) _tmp += " ";
+          _tmp += _cnv;
+        }
+      }
+    }
+
+    if (_tmp) {
+      if (_key) _res += _key;
+      if (_res) _res += " ";
+      _res += _tmp;
+    }
+  }
+
+  return _res;
+}
+
+function _cn_bol(_key: string, _val: boolean) {
+  let _res = "";
+
+  if (_key && _val) {
+    for (const _cnv of _key.split(" ")) {
+      if (!_cnv) continue;
+      if (_res) _res += " ";
+      _res += _cnv;
     }
   }
 
@@ -65,6 +103,7 @@ function _cn_arr(_key: string, _val: string[]): string {
 
 function _cn_obj(_ent: string[] | cobj): string {
   let _res = "";
+  let _tmp = "";
 
   if (Array.isArray(_ent)) {
     _res += _cn_arr("", _ent);
@@ -74,19 +113,24 @@ function _cn_obj(_ent: string[] | cobj): string {
 
       switch (typeof _val) {
         case "string":
+          _tmp = _cn_str(_cn_cln(_key), _val);
+          if (!_tmp) break;
           if (_res) _res += " ";
-          _res += _cn_str(_cn_cln(_key), _val.split(" "));
+          _res += _tmp;
           break;
 
         case "object":
+          _tmp = _cn_arr(_cn_cln(_key), _val);
+          if (!_tmp) break;
           if (_res) _res += " ";
-          _res += _cn_arr(_cn_cln(_key), _val);
+          _res += _tmp;
           break;
 
         default:
-          if (!_val) break;
+          _tmp = _cn_bol(_key, _val);
+          if (!_tmp) break;
           if (_res) _res += " ";
-          _res += _cn_cln(_key);
+          _res += _tmp;
           break;
       }
     }
